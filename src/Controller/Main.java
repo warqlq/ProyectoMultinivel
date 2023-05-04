abc3415ffb077fa46153c8a041e84cb84b87d977
warqlq <davidguerrerinho@gmail.com>
2023-05-04 02:34:09 -0500

    Se agrega controller

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,3 +1,5 @@
+package Controller;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -14,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("hola");
+        System.out.println("buenalrata");
     }
     
 }
